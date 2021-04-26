2dfb9d07a87b3df5b1f9d93e15be1e3de05f3329
Denis <Dragon-F-18@yandex.ru>
2021-04-26 15:27:20 +0700

    DZ36

--- js/anchor.js
+++ js/anchor.js
@@ -26,7 +26,7 @@ $(function() {
 
 let elem = document.querySelector('input[type=tel]');
 let maskOptions = {
-    mask: '+{7(000)}000-00-00'
+    mask: '+{7 (000)} 000-00-00'
 };
 let tel = new IMask(elem, maskOptions);
 
